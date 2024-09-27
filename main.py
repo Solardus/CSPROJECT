179f4f743a2cd77516e78266bc92c1d96282f4f6
Solardus <66323244+Solardus@users.noreply.github.com>
2024-09-27 15:42:02 +0100

    RandX and RandY, movement works with boundary protection
    Exception has occurred: UnboundLocalError
    cannot access local variable 'randX' where it is not associated with a value
      File "\\FS-STUDENT-001\StudentData$\StudentDownloads\13cummingsl\Downloads\main.py", line 49, in printMap
        if y == randX and x == randY:
                ^^^^^
      File "\\FS-STUDENT-001\StudentData$\StudentDownloads\13cummingsl\Downloads\main.py", line 59, in <module>
        printMap(0,0,0,0,0)
    UnboundLocalError: cannot access local variable 'randX' where it is not associated with a value

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from tkinter import *
 import os as os
 from msvcrt import *
 import time
+import math
+import random
 
 os.system("python get-pip.py")
 
-x = 0
-y = 0
-lastPosition = 0
+x = 5
+y = 5
+lastPositionX = 0
+lastPositionY = 0
+coins = 0
 
 board = [['*','*','*','*','*','*','*','*','*','*'],
         ['*','*','*','*','*','*','*','*','*','*'],
@@ -20,24 +24,66 @@ board = [['*','*','*','*','*','*','*','*','*','*'],
         ['*','*','*','*','*','*','*','*','*','*'],
         ['*','*','*','*','*','*','*','*','*','*']]
 #10x10
+def randGen(randXX,randYY):
+    board[randYY][randXX] = '$'
 
-for row in board:
-    for col in row:
-        print(col, end=" ") # print each element separated by space
-    print() # Add newline
+def randomX():
+    randx = random.randint(0,9)
+    return randx
 
-#while True:
-    #if getk
+def randomY():
+    randy = random.randint(0,9)
+    return randy
+
+randX = randomX()
+randY = randomY()
+
+randGen(int(randX),int(randY))
+
+def printMap(lastX, lastY, y, x, coins):
+    board[lastY][lastX] = '*'
+    board[x][y] = '@'
+    for row in board:
+        for col in row:
+            print(col, end=" ") # print each element separated by space
+        if y == randX and x == randY:
+            coins = coins + 1
+            print("Coins: {}".format(coins))
+            y = y - 1
+            x = x - 1
+            randX = randomX()
+            randY = randomY()
+            randGen(randX,randY)
+        print() # Add newline
+
+printMap(0,0,0,0,0)
+
 while True:
     key = ord(getch())
     if key == 75:
         print("Left Arrow")
+        lastPositionX = x
+        lastPositionY = y
+        x = x -1
+        y = y
     if key == 77:
         print("Right Arrow")
+        lastPositionX = x
+        lastPositionY = y
+        x = x +1
+        y = y
     if key == 72:
         print("Up Arrow")
+        lastPositionX = x
+        lastPositionY = y
+        x = x
+        y = y -1
     if key == 80:
         print("Down Arrow")
+        lastPositionX = x
+        lastPositionY = y
+        x = x
+        y = y +1
     if key == 32:
         print("Space Key")
     if key == 13:
@@ -46,4 +92,9 @@ while True:
         print("i key")
     if key == 101:
         print("e key")
+    if x == 10:
+        x = 0
+    if y == 10:
+        y = 0
+    printMap(lastPositionX, lastPositionY, x, y, coins)
     time.sleep(0.1)
